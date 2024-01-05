adb1f1a78bfd25ef3d41649f5acc89ec71d58e5f
Mohabkamel1 <mohabwael896@gmail.com>
2024-01-05 09:15:38 -0800

    7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    default_value = 0
+
+    tuple_a = tuple_a + (default_value,) * (max(len(tuple_a), len(tuple_b)) - len(tuple_a))
+    tuple_b = tuple_b + (default_value,) * (max(len(tuple_a), len(tuple_b)) - len(tuple_b))
+
+    return tuple(a + b for a, b in zip(tuple_a, tuple_b))[:2]
