c55ed05aa0ad3243f4d3e51cbbabf917e378ecb8
Mohabkamel1 <mohabwael896@gmail.com>
2024-01-05 09:24:39 -0800

    9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+def max_integer(my_list=[]):
+  if not my_list:
+    return None
+
+  biggest = my_list[0]
+
+  for element in my_list[1:]:
+    if element > biggest:
+      biggest = element
+
+  return biggest
