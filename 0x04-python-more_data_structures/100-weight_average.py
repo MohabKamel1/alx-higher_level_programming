65120a98531d3a9aa33847288dd38318419b2f3d
Mohabkamel1 <mohabwael896@gmail.com>
2024-01-10 08:19:09 -0800

    100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    num = 0
+    sco = 0
+
+    for tupl in my_list:
+        num += tupl[0] * tupl[l]
+        sco += tupl[1]
+
+    return (num / tupl)
