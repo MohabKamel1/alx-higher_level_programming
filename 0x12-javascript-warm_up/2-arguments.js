34329e88e1e1f52d5beaa7143d357ad071f09b03
MohabKamel1 <mohabwael896@gmail.com>
2024-04-09 01:04:02 +0200

    2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,9 +4,9 @@
 const numArguments = process.argv.length - 2;
 
 // Check the number of arguments and print the corresponding message
-if (numArguments === 0) {
+if (numArguments === 2) {
   console.log("No argument");
-} else if (numArguments === 1) {
+} else if (numArguments === 3) {
   console.log("Argument found");
 } else {
   console.log("Arguments found");
