9abc485406e848037b8a52df0fbaa1621352ce5b
MohabKamel1 <mohabwael896@gmail.com>
2024-04-09 01:33:51 +0200

    2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
-
-// Get the number of arguments passed (excluding the default two arguments)
-const numArguments = process.argv.length.;
-
-// Check the number of arguments and print the corresponding message
-if (numArguments === 2) {
+if (process.argv.length === 2) {
   console.log('No argument');
-} else if (numArguments === 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
-
